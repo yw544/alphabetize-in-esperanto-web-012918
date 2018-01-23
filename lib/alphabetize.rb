e9188c6f709fba942cbefeba46fdc7ba6ac81343
York Wang <yw544@nyu.edu>
2018-01-23 08:50:23 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
 def alphabetize(arr)
   # code here
+  esperanto_alphabet = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arr.sort_by do |element|
+    element.split("").map do |character|
+      esperanto_alphabet.index(character)
+    end
+  end
 end
